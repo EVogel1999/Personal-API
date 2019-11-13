f83b9cbca862246aaa9b5d854f7fe29fb397b482
Emily Vogel <eav990@gmail.com>
2019-11-13 15:25:11 -0500

    Add cors filtering

--- src/main/java/com/example/personal/PersonalApplication.java
+++ src/main/java/com/example/personal/PersonalApplication.java
@@ -2,12 +2,25 @@ package com.example.personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PersonalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalApplication.class, args);
+	}
+	
+	@EnableWebMvc
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
 	}
 
 }
